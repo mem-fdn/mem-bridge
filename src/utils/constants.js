2f7f5585a80c28fc230b5774104373bff6684449
charmful0x <77340894+charmful0x@users.noreply.github.com>
2024-03-05 17:55:15 +0100

    chore: update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
 export const BRIDGES_CONTRACTS = {
-	"0x8ec2333517cc3bFaEC6f8069a46Fba0A5cec912A" : {
+	"0x672FdBa986dbaBCFaC8A4321380dddeC473F8942" : {
 		name: "chainlink_token",
 		decimals: 1e18
 	}
 }
 
-export const MEM_ORACLE_ID = `hJLswl2XkoqHOZlOjDaZmOrolpaIvW8OvtfTA3AbzHs`;
+export const MEM_ORACLE_ID = `UqRY2yV31iWuGc2QZD2_jYIh8IqaZ7aukxob8dDDfqY`;
 
 export const RPC_URL = `https://1rpc.io/sepolia`;
+
+https://molext1.com/solana/spl/XMEHm7wePVSrK8QanGtqBBdEgYmL5CFLvEtCcw92kCVCSzQgmkr7TeWBSLnpSovDmSokyyXC7pnhYE88HSumR7E/7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs/JX73ZkmUZysvbDiHMQuJw9EFz27xys4nLA74wzp99gn/9FJZKLghjhnzXyxvm13v45tpuXj2VCFBxdjpffsuJ5GK
