de0be56d3d8d37c2eac51940c5bf6cfb3e4233ba
charmful0x <77340894+charmful0x@users.noreply.github.com>
2024-05-14 23:45:51 +0200

    chore: change sepolia RPC

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ export const CHAINLINK_TOKEN_BRIDGE =
 export const MEM_ORACLE_ID = `C4hsuvtitlF6I6a92BKteGX5AhjcWEKJB5jgESETI54`;
 export const AO_PROCESS_ID = `xtvzEpBJfkrKz8FRxwFUkP3q5x5OOWDn3LE6bkf0MT0`;
 
-export const RPC_URL = `https://1rpc.io/sepolia`;
+export const RPC_URL = `https://rpc.sepolia.org/`;
 
 export const BRIDGE_ABI = [
   "function validateUnlock(string calldata _memid, address _caller) public returns (bytes32 requestId)",
